f88baaceabf4a348cf51ad779c593b0734305046
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-19 14:09:28 +0700

    red

---
+++ TestTDD/TestTDDTests/TestTDDTests1906.m
@@ -0,0 +1,18 @@
+//
+//  TestTDDTests1906.m
+//  TestTDD
+//
+//  Created by Quach Ngoc Tam on 6/19/13.
+//  Copyright (c) 2013 QsoftVietNam. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+
+SPEC_BEGIN(Calcul)
+    describe(@"test calculator", ^{
+        it(@"test input nil return 0", ^{
+            [[theValue([Obj1906 cal:@""]) should] equal:theValue(0)];
+        });
+    });
+SPEC_END
