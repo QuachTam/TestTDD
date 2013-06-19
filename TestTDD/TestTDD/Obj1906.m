2dc7cb0c7fb435d0e677ea7896bb74e0b06774d7
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-19 14:19:54 +0700

    green

--- TestTDD/TestTDD/Obj1906.m
+++ TestTDD/TestTDD/Obj1906.m
@@ -10,10 +10,15 @@
 
 @implementation Obj1906
 +(NSInteger)cal:(NSString*)strValue{
-    return 3;
     if ([strValue length]==0) {
         return 0;
     }
-    return [strValue integerValue];
+    
+    NSArray *array = [strValue componentsSeparatedByString:@","];
+    NSInteger total =0;
+    for (int i=0; i<[array count]; i++) {
+        total = total + [[array objectAtIndex:i] integerValue];
+    }
+    return total;
 }
 @end
