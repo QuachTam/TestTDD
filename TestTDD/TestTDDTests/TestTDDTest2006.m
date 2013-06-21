dbfcfd3d1d01e7270217eea32f2bc51c76799f67
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-21 14:20:31 +0700

    green

--- TestTDD/TestTDDTests/TestTDDTest2006.m
+++ TestTDD/TestTDDTests/TestTDDTest2006.m
@@ -29,5 +29,8 @@ SPEC_BEGIN(Calcu)
         it(@"content 1\n2,3 equal 6", ^{
             [[theValue([Obj2006 cal2006:@"1\n2,3"]) should] equal:theValue(6)];
         });
+        it(@"content 1,\n equa 1", ^{
+            [[theValue([Obj2006 cal2006:@"1,\n"]) should] equal:theValue(1)];
+        });
     });
 SPEC_END
