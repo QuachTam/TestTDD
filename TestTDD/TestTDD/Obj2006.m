5721f625137d85efaeced1874e19262fc816abd2
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-21 14:38:21 +0700

    red

--- TestTDD/TestTDD/Obj2006.m
+++ TestTDD/TestTDD/Obj2006.m
@@ -10,10 +10,14 @@
 
 @implementation Obj2006
 +(NSInteger)cal2006:(NSString*)strValue{
-    return 3;
     if ([strValue length]==0) {
         return 0;
     }
+    NSArray *array1 = [strValue componentsSeparatedByString:@"\\"];
+    if ([array1 count]>=2) {
+        NSString *delimiter = [array1 objectAtIndex:1];
+    }
+    
     NSArray *arrayTemp = [strValue componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\n ,"]];
     NSInteger total = 0;
     for (NSString *info in arrayTemp) {
