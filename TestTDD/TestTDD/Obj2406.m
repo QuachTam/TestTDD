311d541aeed408f7cfa4c0a5ed7ab2bdb1142e8e
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-24 14:20:55 +0700

    green

--- TestTDD/TestTDD/Obj2406.m
+++ TestTDD/TestTDD/Obj2406.m
@@ -10,13 +10,22 @@
 
 @implementation Obj2406
 +(NSInteger)cal2604:(NSString*)strValue{
-    return 3;
     if ([strValue length]==0) {
         return 0;
     }
-    NSArray *array = [strValue componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\n ,"]];
+    NSString *strtemp = [strValue substringWithRange:NSMakeRange(0, 2)];
+    NSString *delimiter;
+    if ([strtemp isEqualToString:@"//"]) {
+        delimiter = [strValue substringWithRange:NSMakeRange(1, 2)];
+    }else{
+        delimiter = @",";
+    }
+    NSString *subValue = [strValue substringWithRange:NSMakeRange(2, [strValue length]-1)];
+    NSString *delimiterTemp = [NSString stringWithFormat:@"\n %@", delimiter];
+    NSArray *array = [subValue componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:delimiterTemp]];
     NSInteger total = 0;
     
+    //  //;\n1;2
     for (NSString  * value in array) {
         total +=[value integerValue];
     }
