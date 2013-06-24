c2d709c624497ff1e9d4a1a154efa8c7fedb938e
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-24 14:03:45 +0700

    red

--- TestTDD/TestTDDTests/TestTDDTest2402.m
+++ TestTDD/TestTDDTests/TestTDDTest2402.m
@@ -29,5 +29,9 @@ SPEC_BEGIN(Obj24062013)
         it(@"input string 1\n2,3", ^{
             [[theValue([Obj2406 cal2604:@"1\n2,3"]) should] equal: theValue(6)];
         });
+        
+        it(@"input string \\;", ^{
+            [[theValue([Obj2406 cal2604:@"//;\n1;2"]) should] equal: theValue(3)];
+        });
     });
 SPEC_END
