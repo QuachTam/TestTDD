30cc8a6e7096916ea9bcc2e28b85bdf2a37c23c6
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-25 13:44:37 +0700

    red

---
+++ TestTDD/TestTDDTests/TestTDDTest2506.m
@@ -0,0 +1,16 @@
+//
+//  TestTDDTest2506.m
+//  TestTDD
+//
+//  Created by Quach Ngoc Tam on 6/25/13.
+//  Copyright (c) 2013 QsoftVietNam. All rights reserved.
+//
+#import "Kiwi.h"
+
+SPEC_BEGIN(calcu2506)
+    describe(@"calculator string input", ^{
+        it(@"string input nil equal 0", ^{
+            [[theValue([Obj2506 cal2506:@""]) should] equal:theValue(0)];
+        });
+    });
+SPEC_END
