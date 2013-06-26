73e49f545b6bb897e2fbe564e2269a7c8284b8ba
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-26 13:57:24 +0700

    red

--- TestTDD/TestTDDTests/TestTDDTest2606.m
+++ TestTDD/TestTDDTests/TestTDDTest2606.m
@@ -29,5 +29,9 @@ SPEC_BEGIN(calcultor2605)
         it(@"string input 1,2,3 equal 6", ^{
             [[theValue([Obj2605 cal2605:@"1,2,3"]) should] equal:theValue(6)];
         });
+        
+        it(@"string input 1\n2,3 equal 6", ^{
+            [[theValue([Obj2605 cal2605:@"1\n2,3"]) should] equal:theValue(6)];
+        });
     });
 SPEC_END
