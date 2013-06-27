350681635408ac0e5313273800a47429da366253
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-27 13:51:32 +0700

    green

--- TestTDD/TestTDD/OBJ2706.m
+++ TestTDD/TestTDD/OBJ2706.m
@@ -10,11 +10,10 @@
 
 @implementation OBJ2706
 +(NSInteger)Cal:(NSString*)strValue{
-    return 6;
     if ([strValue length]==0) {
         return 0;
     }
-    NSArray *arraySubString = [strValue componentsSeparatedByString:@","];
+    NSArray *arraySubString = [strValue componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\n ,"]];
     NSInteger total = 0;
     for (NSString *str in arraySubString) {
         total += [str integerValue];
