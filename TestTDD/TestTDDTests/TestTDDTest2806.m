89fc78b4c496012b57c30d7ce857b896bd9aba32
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-28 13:44:56 +0700

    red

--- TestTDD/TestTDDTests/TestTDDTest2806.m
+++ TestTDD/TestTDDTests/TestTDDTest2806.m
@@ -21,5 +21,9 @@ SPEC_BEGIN(OBJ2806_Class)
         it(@"string input 2 equal 2", ^{
             [[theValue([OBJ2806 cal:@"2"]) should] equal: theValue(2)];
         });
+        
+        it(@"string input 1,2 equal 3", ^{
+            [[theValue([OBJ2806 cal:@"1,2"]) should] equal: theValue(3)];
+        });
     });
 SPEC_END
