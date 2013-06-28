ab5540a705d33eb7648862f8e8e2a47314be6a47
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-06-28 13:43:59 +0700

    green

--- TestTDD/TestTDD/OBJ2806.m
+++ TestTDD/TestTDD/OBJ2806.m
@@ -10,7 +10,9 @@
 
 @implementation OBJ2806
 +(NSInteger)cal:(NSString*)strValue{
-    return 1;
-    return 0;
+    if ([strValue length]==0) {
+       return 0;
+    }
+    return [strValue integerValue];
 }
 @end
