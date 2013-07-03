dbbad47ea0b66c507b04591fa7039053805debe7
Quach Ngoc Tam <quachngoctam@Quachs-Mac-mini.local>
2013-07-03 13:57:37 +0700

    red

---
+++ TestTDD/TestTDDTests/TestTDDTest0307.m
@@ -0,0 +1,18 @@
+//
+//  TestTDDTest0307.m
+//  TestTDD
+//
+//  Created by Quach Ngoc Tam on 7/3/13.
+//  Copyright (c) 2013 QsoftVietNam. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(class_0307)
+    describe(@"Test TDD 2", ^{
+        it(@"open new account", ^{
+            BankAccount *_bankAcount = [[BankAccount alloc] init];
+            [_bankAcount.accountNumber shouldNotBeNil];
+        });
+    });
+SPEC_END
